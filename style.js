ec69befa8df00b95b5e3b445d48c5d87973c2c38
OndrejSimek <ondrej.simek@g.pslib.cz>
2021-10-22 23:35:05 +0200

    progres

---
+++ style.js
@@ -0,0 +1,10 @@
+function smajlik() {
+    document.getElementsByClassName("svg").style.display = "none";
+    document.getElementById("smile").style.display = "block"
+}
+
+function signature() {
+    document.getElementsByClassName("svg").style.display = "none";
+    document.getElementById("signature").style.display = "block";
+
+}
