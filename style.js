7b35075cde19e6c59e2ac066f61d9b85003d1720
OndrejSimek <ondrej.simek@g.pslib.cz>
2021-10-23 01:10:08 +0200

    dalsi progres

--- style.js
+++ style.js
@@ -1,10 +1,12 @@
 function smajlik() {
-    document.getElementsByClassName("svg").style.display = "none";
-    document.getElementById("smile").style.display = "block"
+    document.getElementById("signature").style.display = "none";
+    document.getElementById("smile").style.display = "block";
+    
 }
 
 function signature() {
-    document.getElementsByClassName("svg").style.display = "none";
+
     document.getElementById("signature").style.display = "block";
+    document.getElementById("smile").style.display = "none";
 
 }
